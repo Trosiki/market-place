80e3ab2f9821caee8f7097e9b46493e888b4f1d3
rafael.moya <rafael.moya@externos.correo.gob.es>
2024-07-24 22:27:01 +0200

    Details product mockup

--- troskishop/src/TroskiShop/Infrastructure/Framework/Symfony/Controller/HomeController.php
+++ troskishop/src/TroskiShop/Infrastructure/Framework/Symfony/Controller/HomeController.php
@@ -5,13 +5,27 @@ namespace TroskiShop\Infrastructure\Framework\Symfony\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use TroskiShop\Domain\Model\Product;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function indexAction(): Response
     {
-        return $this->render('base.html.twig');
+        $products = [];
+        for($i = 0; $i < 20; $i++) {
+            $product = new Product();
+            $product->setName("Nombre ejemplo " . $i);
+            $product->setCategory("Categoria");
+            $product->setDescription("Una descripción algo breve pero a la vez algo larga para ver como queda esta vaina, porque me resulta interesante ver cómo encaja con mucho texto.");
+            $product->setImage('image/ejemplo.png');
+            $product->setPrice(rand(0,1000));
+            $products[] = $product;
+        }
+
+        return $this->render('front_pages/catalog.html.twig', [
+            "products" => $products
+        ]);
     }
 
 }
