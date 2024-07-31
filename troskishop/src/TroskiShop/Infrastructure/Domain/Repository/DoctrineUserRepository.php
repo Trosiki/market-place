a5ea7b00ed148d1f8b11d646bb5a10210d00c441
rafael.moya <rafael.moya@externos.correo.gob.es>
2024-07-31 20:16:38 +0200

    Updating schema and repository

--- troskishop/src/TroskiShop/Infrastructure/Domain/Repository/DoctrineUserRepository.php
+++ troskishop/src/TroskiShop/Infrastructure/Domain/Repository/DoctrineUserRepository.php
@@ -2,13 +2,20 @@
 
 namespace TroskiShop\Infrastructure\Domain\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use TroskiShop\Domain\Model\User;
 use TroskiShop\Domain\Repository\UserRepositoryInterface;
 
-class DoctrineUserRepository extends EntityRepository implements UserRepositoryInterface
+class DoctrineUserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
     const ENTITY_ALIAS = 'USER';
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
 
     public function findByEmail(string $email): ?User
     {
