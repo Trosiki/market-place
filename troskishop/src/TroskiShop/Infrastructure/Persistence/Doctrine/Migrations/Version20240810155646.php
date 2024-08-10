e28a9867819ff0ce39c267b2423926360eafefc3
rafael.moya <rafael.moya@externos.correo.gob.es>
2024-08-10 20:07:03 +0200

    update versions

---
+++ troskishop/src/TroskiShop/Infrastructure/Persistence/Doctrine/Migrations/Version20240810155646.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TroskiShop\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240810155646 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE PRODUCT ADD createdAt DATE NOT NULL, ADD updatedAt DATE NOT NULL, ADD file_name VARCHAR(255) DEFAULT NULL, ADD file_original_name VARCHAR(255) DEFAULT NULL, ADD file_mime_type VARCHAR(255) DEFAULT NULL, ADD file_size INT DEFAULT NULL, ADD file_dimensions LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE PRODUCT DROP createdAt, DROP updatedAt, DROP file_name, DROP file_original_name, DROP file_mime_type, DROP file_size, DROP file_dimensions');
+    }
+}
